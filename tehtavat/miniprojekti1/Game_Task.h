bbdda4a00ff846aac7dcf66388805c569a29636e
Niklas-Malmgren <94905845+Niklas-Malmgren@users.noreply.github.com>
2024-04-02 18:59:15 +0300

    Delete tehtavat/miniprojekti1 directory

--- tehtavat/miniprojekti1/Game_Task.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-class Game_Task
-{
-public:
-	virtual void perform() = 0;
-};
-
--- tehtavat/miniprojekti1/World_Updater.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Game_Task.h"
-#include <iostream>
-
-class World_Updater : public Game_Task
-{
-public:
-	void perform() override;
-};
